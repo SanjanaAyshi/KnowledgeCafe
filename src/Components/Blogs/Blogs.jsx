eb922efb1eef8e4018177ef370ac20cdf9a65444
Sanjana Ayshi <sanjana.ayshi92@gmail.com>
2024-10-24 07:40:57 +0600

    testing and fixing

---
+++ src/Components/Blogs/Blogs.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+
+const Blogs = () => {
+  const [blogs, setBlogs] = useState([]);
+
+  return (
+    <div>
+      <h1 className="text-6xl">blog bolba</h1>
+    </div>
+  );
+};
+
+export default Blogs;
