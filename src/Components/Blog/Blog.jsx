6d0d87622177022dec7a4ab070e361590b03bddf
Sanjana Ayshi <67552924+SanjanaAyshi@users.noreply.github.com>
2024-10-24 16:13:52 +0600

    blog add

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,8 +1,23 @@
 import PropTypes from "prop-types";
 const Blog = ({ blog }) => {
+  const { title, cover_image, name, picture, read_time } = blog;
   console.log(blog);
-  return <div>{/* <h1>blog: {blog.title}</h1> */}</div>;
+  return (
+    <div>
+      <img src={cover_image} alt={`its ${title}`} />
+      <div className="flex">
+        <div className="">
+          <img src={picture} alt={`${name} author`} />
+          <h5>{name}</h5>
+        </div>
+        <div className="">
+          <span>${read_time} long</span>
+        </div>
+      </div>
+      <h1>blog: {title}</h1>
+    </div>
+  );
 };
 
-Blog.PropTypes = { blog: PropTypes.object.isRequired };
+Blog.propTypes = { blog: PropTypes.object.isRequired };
 export default Blog;
--- src/Components/Blogs/Blogs.jsx
+++ src/Components/Blogs/Blogs.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Blog from "../Blog/Blog";
 
 const Blogs = () => {
   const [blogs, setBlogs] = useState([]);
@@ -12,6 +13,9 @@ const Blogs = () => {
   return (
     <div className="md:w-2/3">
       <h1 className="text-6xl">blog bolba : {blogs.length}</h1>
+      {blogs.map((blog) => (
+        <Blog key={blog.id} blog={blog}></Blog>
+      ))}
     </div>
   );
 };
